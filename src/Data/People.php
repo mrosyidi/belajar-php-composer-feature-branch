9b82ef321ac910b6330f1cd64aff300ec69984e4
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-08-22 11:33:05 +0700

    add People.php

---
+++ src/Data/People.php
@@ -0,0 +1,16 @@
+<?php
+
+  namespace ProgrammerZamanNow\Data;
+
+  class People
+  {
+    public function __construct(private string $name)
+    {
+
+    }
+
+    public function sayHello(string $name): string
+    {
+      return "Hello $this->name";
+    }
+  }
